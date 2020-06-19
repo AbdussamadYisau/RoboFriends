6f4ec111d3f493d8bf4f123d4149d9b085c4a2de
AbdussamadYisau <Sammieyisau@gmail.com>
2020-06-19 14:52:40 +0100

    Flex wrap

--- src/CardList.js
+++ src/CardList.js
@@ -5,16 +5,13 @@ import {robots} from './robots';
 
 class CardList extends Component {
     render() {
+        const cardArray = robots.map((user, i) => {
+            return <Card id = {robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        });
         return(
-            <div className = "d-flex">
-                <Card id = {robots[0].id} name={robots[0].name} email={robots[0].email}/>
-                <Card id = {robots[1].id} name={robots[1].name} email={robots[1].email} />
-                <Card id = {robots[2].id} name={robots[2].name} email={robots[2].email}/>
-                <Card id = {robots[3].id} name={robots[3].name} email={robots[3].email}/>
-                <Card id = {robots[4].id} name={robots[4].name} email={robots[4].email} />
-                <Card id = {robots[5].id} name={robots[5].name} email={robots[5].email}/>
-      
-    </div>
+            <div className = "d-flex flex-wrap">
+               {cardArray}
+            </div>
         );
     }
 }
