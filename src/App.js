4946164ac45e1c7507e895d9ca56379ad05e6af9
AbdussamadYisau <Sammieyisau@gmail.com>
2020-06-23 00:45:01 +0100

    Experimenting with Fetch API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots:robots,
+            robots:[],
             searchfield:''
         }
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response=> response.json())
+          .then(users => {this.setState({ robots: users})});
+      }
 
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
     
     render(){
+    
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
